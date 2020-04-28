c6dc1166b735bb0a8909ecb2c81bd4cbf729576a
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-27 23:40:00 -0700

    Add dark mode

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import { Link } from "gatsby";
 import React from "react";
+import DarkModeToggle from "./darkmode";
 
 function Header() {
 
   return (
     <header className="body-font">
-      <div className="flex flex-wrap items-center justify-between max-w-6xl p-4 md:p-8 mx-auto">
+      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 md:p-8 mx-auto">
         <nav
           className={`block flex items-center h-auto w-auto`}
         >
@@ -21,7 +22,7 @@ function Header() {
           ].map((link) => (
             <Link
               activeClassName="border-orange-400 border-b-2"
-              className="block focus:outline-none mr-8 md:mr-16 pb-3 px-0 border-transparent border-b-2 hover:border-orange-400 md:mt-0"
+              className="block focus:outline-none mr-8 md:mr-16 py-3 px-0 border-transparent border-b-2 hover:border-orange-400 md:mt-0"
               key={link.title}
               to={link.route}
             >
@@ -29,6 +30,7 @@ function Header() {
             </Link>
           ))}
         </nav>
+        <DarkModeToggle />
       </div>
     </header>
   );
