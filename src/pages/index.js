1ac1dc45e8ab66ab2ee48dce55802af496e2cec1
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-27 19:32:47 -0700

    Poppins as default font

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Typewriter from 'typewriter-effect';
-
+import Typewriter from "typewriter-effect";
 
 function IndexPage() {
   return (
@@ -13,24 +12,22 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="text-left">
-
-        <h1 className="title-font font-bold sm:text-6xl text-xl">
+      <section className="text-left my-24">
+        <h1 className="title-font font-semibold sm:text-4xl text-xl">
           Hi, I am Chi.
         </h1>
-
-        <div className="text-xl sm:text-6xl font-semibold flex "> 
-      <h1 className="pr-2 sm:pr-4">I am a</h1>
-      <div className="text-orange-400">
-      <Typewriter
-  options={{
-    strings: ['product designer', 'front-end developer'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-</div>
-</div>
+        <div className="text-xl sm:text-4xl font-semibold flex ">
+          <h1 className="pr-2 sm:pr-3 content-center pt-1 sm:py-2">I am a</h1>
+          <div className="font-bold text-orange-400">
+            <Typewriter
+              options={{
+                strings: ["product designer", "front-end developer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
       </section>
     </Layout>
   );
---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography"
+const typography = new Typography({
+  headerFontFamily: [
+    "Poppins",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Poppins", "sans-serif"],
+})
+export default typography
